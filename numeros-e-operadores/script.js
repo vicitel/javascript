096a7bd9e46d081d364969e30d4bb3bbe0483894
Letícia Amaral <100724477+vicitel@users.noreply.github.com>
2023-10-13 23:31:27 -0300

    add: aula 2.3 numeros e operadores

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,27 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total) // 35
+
+// Crie duas expressões que retornem NaN
+var nome = 'leticia 27'
+var soma = nome / 2
+console.log(soma)
+
+var idade = 'vinte e sete'
+var soma2 = idade / 2
+console.log(soma2)
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma3 = +'200' + 50
+console.log(soma3) // 250
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var numero = 5
+console.log(++numero) // 6
+
+// Como dividir o peso por 2?
+var peso = +'80' / 2
+var unidade = 'kg'
+var resultado = peso + unidade
+console.log(resultado) // 40kg
+
